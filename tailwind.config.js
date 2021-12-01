8a96fa157706992615b06e0701f663feacbca692
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2021-12-01 23:34:35 +0700

    change all extensions from .js to .jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/pages/**/*.js', './src/components/**/*.js'],
+  purge: ['./src/pages/**/*.{js,jsx}', './src/components/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
