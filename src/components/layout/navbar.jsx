54442631fe04d2caf595ff03ea36b4c664970147
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2021-12-05 20:24:09 +0700

    apply responsiveness on navbar layout for Desktop devices & create functionality

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -4,8 +4,12 @@ import Image from 'next/image';
 import { GiHamburgerMenu, GiShoppingCart } from 'react-icons/gi';
 import { Button } from '../ui';
 import { navbarLinks } from '../../utils/constant';
+import { useRouter } from 'next/router';
 
 function Navbar() {
+  const router = useRouter();
+  const route = router.pathname;
+
   return (
     <NavbarWrapper>
       <Link href='/'>
@@ -19,13 +23,17 @@ function Navbar() {
         </NavbarBrand>
       </Link>
       <ListContainer>
-        {navbarLinks.map(({ text, link }) => (
-          <li key={text}>
-            <Link href={link}>
-              <ListItem>{text}</ListItem>
-            </Link>
-          </li>
-        ))}
+        {navbarLinks.map(({ text, link }) => {
+          const isCurrentPage = link === route && ListStyle;
+
+          return (
+            <Page key={text}>
+              <Link href={link}>
+                <ListItem className={isCurrentPage}>{text}</ListItem>
+              </Link>
+            </Page>
+          );
+        })}
       </ListContainer>
       <ButtonContainer>
         <Button tw='md:hidden'>
@@ -40,13 +48,15 @@ function Navbar() {
   );
 }
 
-const NavbarWrapper = tw.nav`flex items-center justify-between shadow-md bg-dark px-8`;
-const NavbarBrand = tw.a`relative ml-5 scale-125 top-5 md:ml-0`;
-const ListContainer = tw.ul`hidden text-base md:flex md:items-center space-x-9`;
-const ListItem = tw.a`font-bold text-white`;
+const NavbarWrapper = tw.nav`flex items-center justify-between shadow-md bg-dark px-8 lg:py-5 lg:px-16`;
+const NavbarBrand = tw.a`relative ml-5 scale-125 top-5 md:ml-0 lg:scale-[1.75] lg:top-8`;
+const ListContainer = tw.ul`hidden text-base md:flex md:items-center md:space-x-9 lg:space-x-28`;
+const Page = tw.li`cursor-default relative text-center`;
+const ListItem = tw.a`text-white  cursor-pointer`;
 const ButtonContainer = tw.div`flex items-center bg-dark`;
+const ListStyle = 'font-bold after:border-b-2 after:-translate-x-1/2 after:absolute after:w-3/5 after:-bottom-1 after:left-1/2';
 const LoginButton = tw(
   Button
-)`hidden px-5 py-0.5 font-semibold ml-8 text-dark min-h-[15px] min-w-[15px] md:block text-lg bg-white`;
+)`hidden px-5 py-0.5 font-semibold ml-8 text-dark min-h-[15px] min-w-[15px] md:block text-lg bg-white lg:py-1 px-6`;
 
 export default Navbar;
