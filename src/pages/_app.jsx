e6778f684f514dc002afaa44b04055746b556f38
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2021-12-12 03:54:04 +0700

    add nprogress package for loading indicator

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,37 @@
 import '../styles/globals.css';
+import NProgress from 'nprogress';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { ProductProvider } from '../context/product-context';
 import { Footer, Navbar } from '../components/layout';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => NProgress.start();
+    const handleStop = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
     <ProductProvider>
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
+        />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
